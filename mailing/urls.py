359f9e42e47038a646277e5b3e5ab06e727311a0
Berry_Hunter <tank-20@list.ru>
2026-01-12 23:43:02 +0300

    add CRUD for mailing

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from mailing.apps import MailingConfig
-from .views import RecipientCreateView, RecipientListView
+from .views import RecipientCreateView, RecipientListView, RecipientUpdateView, RecipientDetailView, RecipientDeleteView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('create_recipient/', RecipientCreateView.as_view(), name="create_recipient"),
     path('', RecipientListView.as_view(), name="recipient_list"),
+    path('<int:pk>/', RecipientDetailView.as_view(), name="recipient_detail"),
+    path('<int:pk>/edit/', RecipientUpdateView.as_view(), name="recipient_edit"),
+    path('<int:pk>/delete/', RecipientDeleteView.as_view(), name="recipient_delete"),
 
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, DeleteView, ListView
 from django.views.generic.edit import CreateView, UpdateView
 
@@ -12,10 +13,25 @@ class RecipientCreateView(CreateView):
     model = Recipient
     form_class = RecipientForm
     template_name = 'mailing/recipient_form.html'
-    success_url = '/mailing/'
+    success_url = reverse_lazy("mailing:recipient_list")
 
 class RecipientListView(ListView):
     model = Recipient
     template_name = "mailing/recipient_list.html"
     context_object_name = "recipients"
 
+class RecipientDetailView(DetailView):
+    model = Recipient
+    template_name = 'mailing/recipient_detail.html'
+    context_object_name = "recipient"
+
+class RecipientUpdateView(UpdateView):
+    model = Recipient
+    form_class = RecipientForm
+    template_name = 'mailing/recipient_form.html'
+    success_url = reverse_lazy("mailing:recipient_list")
+
+class RecipientDeleteView(DeleteView):
+    model = Recipient
+    template_name = 'mailing/recipient_confirm_delete.html'
+    success_url = reverse_lazy("mailing:recipient_list")
