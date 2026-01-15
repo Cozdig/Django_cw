65654980ad25fbfcf3db7d2457545afb40c2b930
Berry_Hunter <tank-20@list.ru>
2026-01-15 14:05:16 +0300

    fix mailing urls

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mailing/', include("mailing.urls", namespace="mailing")),
+    path('', include("mailing.urls", namespace="mailing")),
 ]
